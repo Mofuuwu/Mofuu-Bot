dcc78b63944a7793793d4b7c6e9748b40bb303b9
Mofuuwu <Jeangunhildr26@gmail.com>
2024-09-29 11:55:41 +0700

    Update Index.js

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
-const { makeWASocket, DisconnectReason, useMultiFileAuthState } = require('@whiskeysockets/baileys')
+const { makeWASocket, DisconnectReason, useMultiFileAuthState } = require('@whiskeysockets/baileys');
 
-
-
-async function connectToWhatsApp () {
-    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
-    const sock = makeWASocket({
-        // can provide additional config here
+async function connectToWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
+    const conn = makeWASocket({
         printQRInTerminal: true,
         auth: state
-    })
-    sock.ev.on ('creds.update', saveCreds)
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        if(connection === 'close') {
-            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
-            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
-            // reconnect if not logged out
-            if(shouldReconnect) {
-                connectToWhatsApp()
-            }
-        } else if(connection === 'open') {
-            console.log('opened connection')
-        }
-    })
-    sock.ev.on('messages.upsert', async m => {
-        if(m.messages[0].key.fromMe) return
-        console.log(JSON.stringify(m, undefined, 2))
+    });
 
-        console.log('replying to', m.messages[0].key.remoteJid)
-        await sock.sendMessage(m.messages[0].key.remoteJid , { text: 'Haloo' })
-    })
+    conn.ev.on('creds.update', saveCreds);
+    conn.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut;
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect);
+            if (shouldReconnect) {
+                connectToWhatsApp();
+            }
+        } else if (connection === 'open') {
+            console.log('opened connection');
+        }
+    });
+
+    conn.ev.on('messages.upsert', async m => {
+        if (m.messages[0].key.fromMe) return;
+
+        const messageContent = m.messages[0].message.conversation;
+        const senderId = m.messages[0].key.remoteJid;
+        let replyMessage = '';
+
+        console.log("Pesan Baru Diterima");
+        console.log(`${senderId} : ${messageContent}`);
+
+        // Menggunakan if-else untuk pengecekan
+        if (messageContent.toLowerCase() === 'halo' || messageContent.toLowerCase() === 'haloo') {
+            replyMessage = "Haloo jugaa";
+        } else if (messageContent.toLowerCase() === 'menu') {
+            replyMessage = "Mohon Maaf Menu Belum Tersedia";
+        } else if (messageContent.toLowerCase() === 'pagi') {
+            replyMessage = "Selamat Pagi";
+        } else {
+            replyMessage = "Ngomong Apa Yaa??";
+        }
+
+        await conn.sendMessage(senderId, { text: replyMessage });
+    });
 }
 
-connectToWhatsApp()
+connectToWhatsApp();
